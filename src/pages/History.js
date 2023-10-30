d908db79baca0c475c8cb11915e16e3010fb307c
ANU 10 <78339400+ANU1o@users.noreply.github.com>
2023-10-30 09:34:16 +0300

    scroll lock

--- src/pages/History.js
+++ src/pages/History.js
@@ -36,66 +36,68 @@ const History = () => {
         <i class="fa-solid fa-caret-left me-2"></i>Go Back
       </Button>
 
-      <Table hover>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Date</th>
-            <th>Video URL</th>
-            <th className="text-center">Delete</th>
-          </tr>
-        </thead>
-        {history.length > 0 ? (
-          history.map((i) => (
-            <tbody>
-              <tr>
-                <td>{i.videoTitle}</td>
-                <td>{i.date}</td>
-                <td>{i.url}</td>
-                <td className="text-center">
-                  <Button variant="danger" onClick={handleMShow}>
-                    <i className="fa-solid fa-trash"></i>
-                  </Button>
+      <div className="overflow-scroll">
+        <Table hover>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date</th>
+              <th>Video URL</th>
+              <th className="text-center">Delete</th>
+            </tr>
+          </thead>
+          {history.length > 0 ? (
+            history.map((i) => (
+              <tbody>
+                <tr>
+                  <td>{i.videoTitle}</td>
+                  <td>{i.date}</td>
+                  <td>{i.url}</td>
+                  <td className="text-center">
+                    <Button variant="danger" onClick={handleMShow}>
+                      <i className="fa-solid fa-trash"></i>
+                    </Button>
 
-                  <Modal
-                    show={mshow}
-                    backdrop="static"
-                    data-bs-theme="dark"
-                    size="xs"
-                    className="user-select-none"
-                    centered
-                  >
-                    <Modal.Body className="bg-bg text-white">
-                      <Modal.Title className="text-primary">
-                        Confirm Delete?
-                      </Modal.Title>
-                      <Card.Text>
-                        Are you sure to delete this event? This action cannot be
-                        undone!
-                      </Card.Text>
-                    </Modal.Body>
-                    <Modal.Footer className="bg-body border-0">
-                      <Button variant="secondary" onClick={handleMClose}>
-                        <i class="fa-solid fa-xmark me-2"></i>Close
-                      </Button>
-                      <Button
-                        variant="danger"
-                        onClick={() => handleDelete(i.id)}
-                      >
-                        <i class="fa-solid fa-trash me-2"></i>Delete
-                      </Button>
-                    </Modal.Footer>
-                  </Modal>
-                </td>
-              </tr>
-            </tbody>
-          ))
-        ) : (
-          <div className="d-flex flex-column align-items-center justify-content-center">
-            <h1 className="">Nothing Watched Yet.</h1>
-          </div>
-        )}
-      </Table>
+                    <Modal
+                      show={mshow}
+                      backdrop="static"
+                      data-bs-theme="dark"
+                      size="xs"
+                      className="user-select-none"
+                      centered
+                    >
+                      <Modal.Body className="bg-bg text-white">
+                        <Modal.Title className="text-primary">
+                          Confirm Delete?
+                        </Modal.Title>
+                        <Card.Text>
+                          Are you sure to delete this event? This action cannot
+                          be undone!
+                        </Card.Text>
+                      </Modal.Body>
+                      <Modal.Footer className="bg-body border-0">
+                        <Button variant="secondary" onClick={handleMClose}>
+                          <i class="fa-solid fa-xmark me-2"></i>Close
+                        </Button>
+                        <Button
+                          variant="danger"
+                          onClick={() => handleDelete(i.id)}
+                        >
+                          <i class="fa-solid fa-trash me-2"></i>Delete
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+                  </td>
+                </tr>
+              </tbody>
+            ))
+          ) : (
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <h1 className="">Nothing Watched Yet.</h1>
+            </div>
+          )}
+        </Table>
+      </div>
     </Container>
   );
 };
